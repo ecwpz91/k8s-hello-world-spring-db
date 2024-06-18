3d14cf8eb78b54266aa4dac35624363940092c2a
Michael Surbey <ecwpz91@users.noreply.github.com>
2024-06-18 15:17:30 -0400

    Initial revision

---
+++ src/test/java/com/redhat/k8s_hello_world_spring_db/K8sHelloWorldSpringDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redhat.k8s_hello_world_spring_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sHelloWorldSpringDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
